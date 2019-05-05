341097987e9e529404cf6b42a7f41611ccf06ccb
Darlan Alves Jurak <darlan.jurak@gmail.com>
2019-05-05 15:31:08 -0300

    added monitor

--- minimal/tb.sv
+++ minimal/tb.sv
@@ -9,7 +9,7 @@ Alexandre Amory
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-interface add_sub_if;
+interface add_sub_if(input logic clk, reset);
 
   //Control Information
   bit doAdd;
@@ -124,8 +124,29 @@ class add_sub_driver extends uvm_driver #(add_sub_seq_item);
     
   endtask //drive
   
-
 endclass //add_sub_driver extends uvm_driver
+
+class add_sub_monitor extends uvm_monitor;
+
+  `uvm_component_utils(add_sub_monitor);
+
+  virtual add_sub_if vif;
+  uvm_analysis_port #(add_sub_seq_item) item_collected_port;
+  add_sub_seq_item trans_collected;
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(virtual add_sub_if)::get(this, "", "vif", vif))
+      `uvm_fatal("NO_VIF", {"Virtual interface must be set for:", get_full_name(),".vif"})
+  endfunction : build_phase
+  
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+    trans_collected = new();
+    item_collected_port = new("item_collected_port", this);
+  endfunction //new()
+
+endclass //add_sub_monitor extends uvm_monitor
 
 
 
