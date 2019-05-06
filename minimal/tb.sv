6310dec227c66b1905ca5f311600a2cac2ebddd4
DarlanJurak <darlan.jurak@gmail.com>
2019-05-06 16:16:08 -0300

    added agent, scoreboard, env, and test

--- minimal/tb.sv
+++ minimal/tb.sv
@@ -9,7 +9,7 @@ Alexandre Amory
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-interface add_sub_if(input logic clk, reset);
+interface add_sub_if(input logic clk);
 
   //Control Information
   bit doAdd;
@@ -19,8 +19,26 @@ interface add_sub_if(input logic clk, reset);
   //Analysis Information
   bit [8:0] result;
 
-  modport DRIVER  (output doAdd,  a,  b);
-  modport MONITOR (input  result);
+  clocking cb_driver @(posedge clk);
+
+    //Control Information
+    output doAdd;
+    //Payload Information
+    output a;
+    output b;
+
+  endclocking  
+
+  clocking cb_monitor @(posedge clk);
+
+    //Analysis Information
+    input result;
+
+  endclocking  
+
+  // ??
+  modport DRIVER  (clocking cb_driver);
+  modport MONITOR (clocking cb_monitor);
   
 endinterface //add_sub_if
 
@@ -120,12 +138,18 @@ class add_sub_driver extends uvm_driver #(add_sub_seq_item);
   virtual task drive();
 
     req.print();
-    // `DRIV_IF.
+    @(posedge vif.clk);
+      vif.cb_driver.doAdd  <= req.doAdd;
+      vif.cb_driver.a      <= req.a;
+      vif.cb_driver.b      <= req.b;
     
   endtask //drive
   
 endclass //add_sub_driver extends uvm_driver
 
+//----------------
+// monitor add_sub_monitor
+//----------------
 class add_sub_monitor extends uvm_monitor;
 
   `uvm_component_utils(add_sub_monitor);
@@ -148,75 +172,197 @@ class add_sub_monitor extends uvm_monitor;
 
 endclass //add_sub_monitor extends uvm_monitor
 
+//----------------
+// agent add_sub_agent
+//----------------
+class add_sub_agent extends uvm_agent;
+
+  `uvm_component_utils(add_sub_agent)
+
+  add_sub_driver    driver;
+  add_sub_sequencer sequencer;
+  add_sub_monitor   monitor;
+  
+  // Build phase
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    if(get_is_active() == UVM_ACTIVE)begin
+      driver    = add_sub_driver::type_id::create("driver", this);
+      sequencer = add_sub_sequencer::type_id::create("sequencer", this);
+    end
+
+    monitor = add_sub_monitor::type_id::create("monitor", this);
+    
+  endfunction
+
+  // Connect phase
+  function void connect_phase(uvm_phase phase);
+
+    if(get_is_active() == UVM_ACTIVE) begin
+      driver.seq_item_port.connect(sequencer.seq_item_export);
+    end
+    
+  endfunction
+
+  // Constructor
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction //new()
+
+endclass //add_sub_agent extends uvm_agent
+
+//----------------
+// scoreboard add_sub_scoreboard
+//----------------
+class add_sub_scoreboard extends uvm_scoreboard;
+
+  `uvm_component_utils(add_sub_scoreboard);
+  //Declare port
+  uvm_analysis_imp#(add_sub_seq_item, add_sub_scoreboard) item_collected_export;
+  
+  //Constructor
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    //Creating
+    item_collected_export = new("item_collected_export", this);
+
+  endfunction
+
+  virtual function void write(add_sub_seq_item pkt);
+    $display("SCB:: Packet received:");
+    pkt.print();
+  endfunction
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction //new()
+
+endclass //add_sub_scoreboard extends uvm_scoreboard
+
+//----------------
+// environment add_sub_env
+//----------------
+// class add_sub_env extends uvm_env;
+
+//   `uvm_component_utils(add_sub_env);
+
+//   add_sub_agent agent;
+
+//   function void build_phase(uvm_phase phase);
+//     super.build_phase(phase);
+    
+//     agent = add_sub_agent::type_id::create("add_sub_agent", this);
+
+//   endfunction
+  
+//   function new(string name, uvm_component parent);
+//     super.new(name, parent);
+//   endfunction //new()
+
+// endclass //add_sub_env extends uvm_env
+
+//----------------
+// test
+//----------------
+// class add_sub_test extends uvm_test;
+ 
+//   `uvm_component_utils(add_sub_test)
+ 
+//   add_sub_env       env;
+//   add_sub_sequence  seq;
+ 
+//   function new(string name = "add_sub_test",uvm_component parent=null);
+//     super.new(name,parent);
+//   endfunction : new
+ 
+//   virtual function void build_phase(uvm_phase phase);
+//     super.build_phase(phase);
+ 
+//     env = add_sub_env::type_id::create("env", this);
+//     seq = add_sub_sequence::type_id::create("seq");
+
+//   endfunction : build_phase
+ 
+//   task run_phase(uvm_phase phase);
+//     seq.start(env.agent.sequencer);
+//   endtask : run_phase
+ 
+// endclass : add_sub_test
+
 
 
 //----------------
 // environment env
 //----------------
-// class env extends uvm_env;
+class env extends uvm_env;
 
-//   virtual add_sub_if m_if;
+  virtual add_sub_if m_if;
 
-//   function new(string name, uvm_component parent = null);
-//     super.new(name, parent);
-//   endfunction
+  function new(string name, uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
   
-//   function void connect_phase(uvm_phase phase);
-//     `uvm_info("LABEL", "Started connect phase.", UVM_HIGH);
-//     // Get the interface from the resource database.
-//     assert(uvm_resource_db#(virtual add_sub_if)::read_by_name(get_full_name(), "add_sub_if", m_if));
-//     `uvm_info("LABEL", "Finished connect phase.", UVM_HIGH);
-//   endfunction: connect_phase
+  function void connect_phase(uvm_phase phase);
+    `uvm_info("LABEL", "Started connect phase.", UVM_HIGH);
+    // Get the interface from the resource database.
+    assert(uvm_resource_db#(virtual add_sub_if)::read_by_name(get_full_name(), "add_sub_if", m_if));
+    `uvm_info("LABEL", "Finished connect phase.", UVM_HIGH);
+  endfunction: connect_phase
 
-//   task run_phase(uvm_phase phase);
-//     phase.raise_objection(this);
-//     `uvm_info("LABEL", "Started run phase.", UVM_HIGH);
-//     begin
-//       int a = 8'h2, b = 8'h3; // applies 2 + 3 as stimuli
-//       @(m_if.cb);
-//       m_if.cb.a <= a;
-//       m_if.cb.b <= b;
-//       m_if.cb.doAdd <= 1'b1;
-//       repeat(2) @(m_if.cb);
-//       `uvm_info("RESULT", $sformatf("%0d + %0d = %0d",
-//         a, b, m_if.cb.result), UVM_LOW); // gets 5 as response
-//     end
-//     `uvm_info("LABEL", "Finished run phase.", UVM_HIGH);
-//     phase.drop_objection(this);
-//   endtask: run_phase
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    `uvm_info("LABEL", "Started run phase.", UVM_HIGH);
+    begin
+      int a = 8'h2, b = 8'h3; // applies 2 + 3 as stimuli
+      @(m_if.cb_driver);
+      m_if.cb_driver.a <= a;
+      m_if.cb_driver.b <= b;
+      m_if.cb_driver.doAdd <= 1'b1;
+      repeat(2) @(m_if.cb_driver);
+      `uvm_info("RESULT", $sformatf("%0d + %0d = %0d",
+        a, b, m_if.result), UVM_LOW); // gets 5 as response
+    end
+    `uvm_info("LABEL", "Finished run phase.", UVM_HIGH);
+    phase.drop_objection(this);
+  endtask: run_phase
   
-// endclass
+endclass
 
 //-----------
 // module top
 //-----------
 module top;
 
-  bit clk;
-  // add_sub_if duv_if(clk);
-  // env environment;
-  //ADD_SUB dut(.clk (clk));
+  bit         clk;
+  bit         rst;
+  add_sub_if  duv_if(clk);
+  // add_sub_env environment;
+  env         env;
 
-  // ADD_SUB dut(
-  // .clk(clk),
-  // .a0(duv_if.a),
-  // .b0(duv_if.b),
-  // .doAdd0(duv_if.doAdd),
-  // .result0(duv_if.result)
-  // );
+  ADD_SUB dut(
+  .clk(clk),
+  .a0(duv_if.a),
+  .b0(duv_if.b),
+  .doAdd0(duv_if.doAdd),
+  .result0(duv_if.result)
+  );
 
   add_sub_seq_item seq_item;
 
   initial begin
-    // environment = new("env");
-    // // Put the interface into the resource database.
-    // uvm_resource_db#(virtual add_sub_if)::set("env", "add_sub_if", duv_if);
-    // clk = 0;
-    // run_test();
 
-    seq_item = add_sub_seq_item::type_id::create();
-    seq_item.randomize();
-    seq_item.print();
+    env = new("env");
+
+    // Put the interface into the resource database.
+    uvm_resource_db#(virtual add_sub_if)::set("env", "add_sub_if", duv_if);
+    clk = 0;
+    run_test();
+
+    // seq_item = add_sub_seq_item::type_id::create();
+    // seq_item.randomize();
+    // seq_item.print();
 
   end
   
@@ -232,3 +378,41 @@ module top;
   end
   
 endmodule
+
+// module top;
+   
+//   //clock and reset signal declaration
+//   bit clk;
+//   bit reset;
+   
+//   //clock generation
+//   always #5 clk = ~clk;
+   
+//   //reset Generation
+//   initial begin
+//     reset = 1;
+//     #5 reset =0;
+//   end
+   
+//   //creatinng instance of interface, inorder to connect DUT and testcase
+//   add_sub_if duv_if(clk,reset);
+   
+//   //DUT instance, interface signals are connected to the DUT ports
+//   ADD_SUB dut(
+//   .clk(clk),
+//   .a0(duv_if.a),
+//   .b0(duv_if.b),
+//   .doAdd0(duv_if.doAdd),
+//   .result0(duv_if.result)
+//   );
+   
+//   //enabling the wave dump
+//   initial begin
+//     uvm_config_db#(virtual add_sub_if)::set(uvm_root::get(),"*","vif",duv_if);
+//     $dumpfile("dump.vcd"); $dumpvars;
+//   end
+   
+//   initial begin
+//     run_test();
+//   end
+// endmodule
